68187b6d4c6558a081d42fad657247ee60863528
Sarah Best <sarahbest910@gmail.com>
2023-08-08 19:49:40 +0000

    Completed String Is Palindrome

--- src/main/java/Palindrome.java
+++ src/main/java/Palindrome.java
@@ -12,6 +12,15 @@ public class Palindrome {
      * @return true if str is a palindrome, false otherwise.
      */
     public boolean pal(String str){
-        return false;
+        String reverse = "";
+        boolean response =  false;
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reverse = reverse + str.charAt(i);
+        }
+        if (str.equals(reverse)) {
+            response = true;
+        }
+        return response;
     }
 }
